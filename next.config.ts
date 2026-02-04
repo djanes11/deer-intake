91694a931d3b60d69365f8ddee8350cab56d503e
djanes11 <djanes11@gmail.com>
2026-02-04 15:05:23 -0500

    update

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ import type { NextConfig } from 'next';
 const config: NextConfig = {
   // keep your setting
   typedRoutes: false,
-  experimental: {
-    typedRoutes: false,
-  },
   // no outputFileTracingIncludes, no externalPackages
 };
 
